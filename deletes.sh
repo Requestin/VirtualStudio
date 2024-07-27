71d3a770f896df0c69d3d032ba2b530b14cc652e
Requestin <darchinyanckaren@gmail.com>
2024-07-28 02:46:41 +0300

    clear branch

--- deletes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Получить список веток, которых уже нет на внешнем сервере
-missing_branches=$(git branch -vv | grep ': gone]' | awk '{print $1}')
-
-# Удалить каждую локальную ветку, у которой удалена её вышестоящая ветка.
-for branch in $missing_branches; do
-    git branch -D $branch
-done
