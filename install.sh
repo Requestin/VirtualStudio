81c27f10fce519e7ba50e98204aff8f7406e9a79
Gyhyry <gyhyry@yandex.ru>
2024-09-03 02:20:55 +0300

    add installing script for windows

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Функция для создания директорий
 create_directories() {
     if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-        mkdir -p data\proxy data\uploads
+        mkdir -p data/proxy data/uploads
     else
         mkdir -p data/proxy data/uploads
     fi
@@ -13,7 +13,7 @@ create_directories() {
 create_and_activate_venv() {
     if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
         python -m venv venv
-        source venv/Scripts/activate
+        .\venv\Scripts\activate
     else
         python3 -m venv venv
         source venv/bin/activate
